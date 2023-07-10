2ee884aab67680d6b2a6d97b7f174cdfab40b390
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-10 15:56:01 -0300

    Adicionando o projeto

---
+++ src/main/java/br/com/rodrigoamora/AplicacaoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoSpringApplication.class, args);
+	}
+
+}
