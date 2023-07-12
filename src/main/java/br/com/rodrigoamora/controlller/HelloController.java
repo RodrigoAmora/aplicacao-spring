d79fd5e9cc6f6ca498637cc36207eff461486fc5
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-11 21:16:08 -0300

    Buscando os pedidos do banco

--- src/main/java/br/com/rodrigoamora/controlller/HelloController.java
+++ src/main/java/br/com/rodrigoamora/controlller/HelloController.java
@@ -1,14 +1,16 @@
 package br.com.rodrigoamora.controlller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 	
 	@GetMapping("/")
-	public String index() {
-		return "index.html";
+	public String index(Model model) {
+		model.addAttribute("name", "Rodrigo");
+		return "index";
 	}
 
 }
