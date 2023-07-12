5b1cbcf92f1d9d5f3fdeb02d9e282b4222f8dc93
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-11 22:32:53 -0300

    Adicionando @RequestMapping

--- src/main/java/br/com/rodrigoamora/controlller/HomeController.java
+++ src/main/java/br/com/rodrigoamora/controlller/HomeController.java
@@ -6,18 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.rodrigoamora.model.Pedido;
 import br.com.rodrigoamora.repository.PedidoRepository;
 
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
 	@Autowired
 	private PedidoRepository repository;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 		
 		List<Pedido> pedidos = repository.findAll();
