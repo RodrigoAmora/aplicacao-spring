52b7f7c1a87dde668669b8c3b4ced6722c9f6896
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-11 21:17:20 -0300

    Buscando pedidos do banco

--- src/main/java/br/com/rodrigoamora/controlller/HomeController.java
+++ src/main/java/br/com/rodrigoamora/controlller/HomeController.java
@@ -1,26 +1,26 @@
 package br.com.rodrigoamora.controlller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.rodrigoamora.model.Pedido;
+import br.com.rodrigoamora.repository.PedidoRepository;
+
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private PedidoRepository repository;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeDoProduto("MacBook Air 13");
-		pedido.setUrlImage("https://m.media-amazon.com/images/I/51ppYyWOrNL._AC_SX679_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Notebook-Apple-MacBook-Air-13-polegadas/dp/B0B3CQVB62/ref=sr_1_2_sspa?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=343CL7PWUJDBE&keywords=macbook&psr=PDAY&qid=1689101607&s=prime-day&sprefix=macbook%2Cprime-day%2C204&sr=1-2-spons&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1&m=A1ZZFT5FULY4LN&SPES=1");
-		pedido.setDescricao("descição.....");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = repository.findAll();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
