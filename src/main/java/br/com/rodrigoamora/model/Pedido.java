d7bd220f83c9cf1b06c805b25480607866a3932d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-11 22:32:03 -0300

    Ajustes no Id

--- src/main/java/br/com/rodrigoamora/model/Pedido.java
+++ src/main/java/br/com/rodrigoamora/model/Pedido.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 public class Pedido {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String nomeDoProduto;
