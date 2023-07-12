fb6e91aece07d33cb8c909d830b38ffa6c8e1740
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-12 15:47:23 -0300

    Criando o metodo de busca de peido pelo status

--- src/main/java/br/com/rodrigoamora/repository/PedidoRepository.java
+++ src/main/java/br/com/rodrigoamora/repository/PedidoRepository.java
@@ -1,9 +1,16 @@
 package br.com.rodrigoamora.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.rodrigoamora.model.Pedido;
+import br.com.rodrigoamora.model.StatusPedido;
 
+@Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	List<Pedido> findByStatus(StatusPedido aguardando);
+	
 }
